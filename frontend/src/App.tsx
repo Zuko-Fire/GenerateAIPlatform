ecf6921f75297964fc0af5e4f502d458697ef023
us_dd <us_dd@nlmk.com>
2024-11-25 23:31:50 +0300

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-// Define types for state variables
-type Data = string | null;
-type Error = string | null;
+import { Auth } from './common/auth/auth';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from './common/config/react-query';
+import { routes } from './common/config/routeConfig';
+import { OpenModal } from './common/components/openModal';
+import { RouterProvider } from 'react-router-dom';
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Data>(null);
-  const [error, setError] = useState<Error>(null);
-
-  useEffect(() => {
-    // Define an asynchronous function to fetch data
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<string>('http://localhost:3001/');
-        setData(response.data);
-      } catch (err) {
-        setError('Error fetching data');
-        console.error(err);
-      }
-    };
-
-    // Call the asynchronous function
-    fetchData();
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-        {data && <p>Data from backend: {data}</p>}
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </header>
-    </div>
+    <Auth>
+    <QueryClientProvider client={queryClient}>
+      <OpenModal />
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  </Auth>
   );
 };
 
